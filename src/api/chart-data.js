7f9eb4a9c32ce0f870a416aa35696590203a983b
dark0n3 <dark.sonik1998@gmail.com>
2020-05-04 10:33:33 +0300

    Add data and integration with graph

--- src/api/chart-data.js
+++ src/api/chart-data.js
@@ -1,4 +1,7 @@
+import data from './data.json'
+
 var barOptions_stacked = {
+    legend: false,
     tooltips: {
         enabled: false
     },
@@ -26,7 +29,6 @@ var barOptions_stacked = {
         }],
         yAxes: [{
             gridLines: {
-                display:false,
                 color: "#fff",
                 zeroLineColor: "#fff",
                 zeroLineWidth: 0
@@ -40,24 +42,47 @@ var barOptions_stacked = {
     },
 };
 
+var current = {...data.mo.current}
+var next = {...data.mo.next}
+var following = {...data.mo.following}
 export const planetChartData = {
     type: 'horizontalBar',
     data: {
         labels: ["current", "next", "after"],
         
         datasets: [{
-            data: [727, 589, 537],
-            backgroundColor: "rgba(63,103,126,1)",
-            hoverBackgroundColor: "rgba(50,90,100,1)"
-        },{
-            data: [238, 553, 746],
+            data: [[current.certain.from, current.certain.to],
+                    [next.certain.from, next.certain.to],
+                    [following.certain.from, following.certain.to],
+                  ],
+                    backgroundColor: "rgba(63,103,126,1)",
+                    hoverBackgroundColor: "rgba(50,90,100,1)"
+        },
+        {
+            data: [
+                current.expected.to-current.certain.to,
+                next.expected.to-next.certain.to, 
+                following.expected.to-following.certain.to,],
             backgroundColor: "rgba(163,103,126,1)",
             hoverBackgroundColor: "rgba(140,85,100,1)"
-        },{
-            data: [1238, 553, 746],
+        },
+        {
+            data: [
+                current.unlikely.to-current.expected.to,
+                next.unlikely.to-next.expected.to,
+                following.unlikely.to-following.expected.to],
             backgroundColor: "rgba(63,203,226,1)",
             hoverBackgroundColor: "rgba(46,185,235,1)"
-        }]
+        },
+        {
+            data: [
+                {x: 30, y:'current'},
+                {x: 30, y:'next'},
+                {x: 30, y:'unlikely'},
+            ],
+            type: 'line'
+        }
+    ]
     },
     options: barOptions_stacked
   }
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,7 +1,23 @@
 <template>
-  <div style="width: 400px"> 
-    <canvas id="planet-chart"></canvas>
-    <canvas id="planet-chart-two"></canvas>
+  <div> 
+    <div class="header">
+      <h2> Where Are We Going to Land </h2>
+      <span class="dots">
+        <span class="certain"> certain </span>
+        <span class="expected"> exoected </span>
+        <span class="unlikely"> unlikely </span>
+      </span>
+      <router-link :to="'/'">
+        <button class="navButton" :class="{active: active === 'Sales'}">Sales</button>
+      </router-link>
+      <router-link :to="'/'">
+        <button class="navButton">Prospecting</button>
+      </router-link>
+    </div>
+    <div style="width: 400px">
+      <canvas id="planet-chart"></canvas>
+      <canvas id="planet-chart-two" class="second-part"></canvas>
+    </div>
   </div>
 </template>
 
@@ -14,6 +30,7 @@ import planetChartDataTwo from '../api/chart-data-two.js';
 export default {
   data() {
     return {
+      active: 'Sales',
       planetChartData: planetChartData,
       planetChartDataTwo: planetChartDataTwo,
     }
@@ -32,11 +49,61 @@ export default {
   },
   mounted() {
     this.createChart('planet-chart', this.planetChartData);
-    this.createChart('planet-chart-two', this.planetChartDataTwo)
+    this.createChart('planet-chart-two', this.planetChartDataTwo);
+    console.log(this.active)
   }
 }
 </script>
 
-<style lang="scss">
+<style>
+  .second-part {
+    margin-top: -13px;
+    border-top: 2px dashed grey;
+  }
+
+  .header {
+    display: flex;
+  }
+
+  .header > .dots {
+    display: flex;
+    align-items: center;
+  }
+
+  .header > .dots > span{
+    width: 70px;
+    max-height: 20px;
+    border-radius: 10px;
+    margin: 0 5px;
+  }
+
+  .header > .dots > .certain {
+    background-color: rgba(63,103,126,1);
+  }
+
+  .header > .dots > .expected {
+    background-color: rgba(163,103,126,1);
+  }
+
+  .header > .dots > .unlikely {
+    background-color: rgba(63,203,226,1);
+  }
+
+  .navButton {
+    background-color: white; /* Green */
+    border: none;
+    color: black;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border: 2px solid rgb(168, 112, 112);
+    width: 200px
+  }
+  .active {
+    background-color: aquamarine;
+  }
+
 
 </style>
